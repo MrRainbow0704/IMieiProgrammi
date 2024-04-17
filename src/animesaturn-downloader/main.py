4429314e7a319b02d361b21c88df39dfede57c94
MrRainbow0704 <2007.marcosimone@gmail.com>
2024-04-17 17:28:41 +0200

    Added session behavinor

--- src/animesaturn-downloader/main.py
+++ src/animesaturn-downloader/main.py
@@ -3,29 +3,32 @@ Tutti i diritti riservati.
 Versione 2.1
 """
 
-import logging, re, requests, time
+import argparse, logging, re, requests, time
 from pathlib import Path
+from typing import TypeAliasType
 from os import getcwd
 
 
-def get_episode_links(url: str) -> list[str]:
+SupportsSlice = TypeAliasType("SupportsSlice", list | tuple)
+Url = TypeAliasType("Url", str | bytes)
+
+
+def get_episode_links(session: requests.Session, url: Url) -> list[Url]:
     linkRegexp = r'<a[^>]*class=["\'][^>]*bottone-ep[^>]*["\'][^>]*[^>]*>'
     hrefRegexp = r'(?<=href=["\']).*?(?=["\'])'
     zeroEpRegexp = r"[^0-9A-Za-z\n]*ep-0[^0-9A-Za-z\n]*"
 
-    res = requests.get(url)
+    res = session.get(url)
     if res.ok:
         # Prendi gli url dai tag <a> estraendoli con delle RegExp
         content = res.text.replace(" ", "")
-        links: list[str] = re.findall(linkRegexp, content, re.IGNORECASE)
-        links: list[str] = [
-            re.findall(hrefRegexp, link, re.IGNORECASE)[0] for link in links
-        ]
+        links = re.findall(linkRegexp, content, re.IGNORECASE)
+        links = [re.findall(hrefRegexp, link, re.IGNORECASE)[0] for link in links]
 
         # Gestione del caso limite "episodio 0"
         if re.findall(zeroEpRegexp, links[0], re.IGNORECASE):
             return links
-        return links.insert(0, "")
+        return ["NOT FOUND"] + links
     else:
         logging.warning(
             f"Ricevuto un codice diverso da 200 OK: {res.status_code}. {res.content}"
@@ -35,11 +38,11 @@ def get_episode_links(url: str) -> list[str]:
         )
 
 
-def get_stream_link(url: str, i: int) -> tuple[int | str]:
+def get_stream_link(session: requests.Session, url: Url, i: int) -> tuple[int, Url]:
     linkRegexp = r'<a[^>]*href=["\'][^>]*watch\?[^>]*["\'][^>]*>'
     hrefRegexp = r'(?<=href=["\']).*?(?=["\'])'
 
-    res = requests.get(url)
+    res = session.get(url)
     if res.ok:
         # Estrai gli url dai tag <a> con delle RegExp
         content = res.text.replace(" ", "")
@@ -56,11 +59,11 @@ def get_stream_link(url: str, i: int) -> tuple[int | str]:
         )
 
 
-def get_video_link(url: str, i: int) -> tuple[int | str]:
+def get_video_link(session: requests.Session, url: Url, i: int) -> tuple[int, Url]:
     sourceRegexp = r"<source[^>]*>"
     srcRegexp = r'(?<=src=["\']).*?(?=["\'])'
 
-    res = requests.get(url)
+    res = session.get(url)
     if res.ok:
         # Estrai gli url dal tag <source> con delle RegExp
         content = res.text.replace(" ", "")
@@ -77,9 +80,11 @@ def get_video_link(url: str, i: int) -> tuple[int | str]:
         )
 
 
-def download_videos(urls: list[tuple[int | str]], path: Path, filename: str) -> None:
+def download_videos(
+    session: requests.Session, urls: list[tuple[int, Url]], path: Path, filename: str
+) -> None:
     # Crea una lista di stream di risposte e una di stream di scrittura di bytes
-    responses = {i: requests.get(url, stream=True) for i, url in urls}
+    responses = {i: session.get(url, stream=True) for i, url in urls}
     streams = {i: responses[i].iter_content(chunk_size=1024) for i, _ in urls}
     handles = {i: open(path / f"{filename}{i}.mp4", "wb") for i, _ in urls}
 
@@ -107,42 +112,110 @@ def download_videos(urls: list[tuple[int | str]], path: Path, filename: str) -> 
                 raise Exception(e)
 
 
-def split_list(lst, chunk_size):
+def split_list(lst: SupportsSlice, chunk_size: int) -> list[SupportsSlice]:
     # Divide una lista in liste più piccole di dimensione `chunk_size` o inferiori
     return [lst[i : i + chunk_size] for i in range(0, len(lst), chunk_size)]
 
 
-def main() -> None:
+def main(cmd: bool) -> None:
     # Prendi variabili dall'utente
-    ROOT = Path(getcwd())
-    url = input("Inserisci il link alla pagina dell'anime: ")
-    primo = int(input("Inserisci il numero del primo episodio da scaricare: "))
-    ultimo = int(input("Inserisci il numero dell'ultimo episodio da scaricare: ")) + 1
-    path = ROOT / input(
-        f'Inserisci il percorso dove salvare i file [Vuoto per: "{ROOT}"]: '
-    )
-    path.mkdir(parents=True, exist_ok=True)
-    filename = input("Inserisci il nome per i file: ") 
-    s = time.perf_counter()
-    
-    # Esegui lo scraping della pagina per poi scaricare tutti i video
-    episodi = get_episode_links(url)
-    epLinks = [
-        get_stream_link(ep, i) for i, ep in enumerate(episodi[primo:ultimo], primo)
-    ]
-    videoLinks = [get_video_link(ep, i) for i, ep in epLinks]
-    for vidLinks in split_list(videoLinks, 3):
-        download_videos(vidLinks, path, filename)
+    if not cmd:
+        url = input("Inserisci il link alla pagina dell'anime: ")
+        primo = input("Inserisci il numero del primo episodio da scaricare: ")
+        try:
+            primo = int(primo)
+        except Exception:
+            primo = None
+        ultimo = input("Inserisci il numero dell'ultimo episodio da scaricare: ")
+        try:
+            ultimo = int(ultimo) + 1
+        except Exception:
+            ultimo = None
+        path = ROOT / input(
+            f'Inserisci il percorso dove salvare i file [Vuoto per: "{ROOT}"]: '
+        )
+        filename = input("Inserisci il nome per i file: ")
+    else:
+        url = args.url
+        primo = args.first
+        try:
+            primo = int(primo)
+        except Exception:
+            primo = None
+        ultimo = args.last
+        try:
+            ultimo = int(ultimo) + 1
+        except Exception:
+            ultimo = None
+        path = ROOT / args.dir
+        filename = args.file_name
 
-    s1 = time.perf_counter() - s
+    path.mkdir(parents=True, exist_ok=True)
+    s = time.perf_counter()
+
+    # Esegui lo scraping della pagina per poi scaricare tutti i video
+    with requests.Session() as session:
+        episodi = get_episode_links(session, url)
+        if episodi[0] == "NOT FOUND" and primo is None:
+            primo = 1
+        epLinks = [
+            get_stream_link(session, ep, i)
+            for i, ep in enumerate(episodi[primo:ultimo], primo)
+        ]
+        videoLinks = [get_video_link(session, ep, i) for i, ep in epLinks]
+        for vidLinks in split_list(videoLinks, 3):
+            download_videos(session, vidLinks, path, filename)
+
+        s1 = time.perf_counter() - s
     logging.info(f"Operazione terminata in {s1:0.2f} secondi.")
 
 
 if __name__ == "__main__":
+    ROOT = Path(getcwd())
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s] %(levelname)s: %(message)s",
         datefmt="%d/%m/%Y %H:%M:%S",
     )
     print(__doc__)
-    main()
+    parser = argparse.ArgumentParser("AnimeSaturn-Downloader")
+    parser.add_argument(
+        "-u",
+        "--url",
+        type=str,
+        default=False,
+        help="L'url per la front page dell'anime. [RICHIESTO]",
+    )
+    parser.add_argument(
+        "-f",
+        "--first",
+        type=int,
+        default=None,
+        help="Il numero del primo episodio da scaricare. [Default: Primo episodio]",
+    )
+    parser.add_argument(
+        "-l",
+        "--last",
+        type=int,
+        default=None,
+        help="Il numero dell'ultimo episodio da scaricare. [Default: Ultimo episodio]",
+    )
+    parser.add_argument(
+        "-d",
+        "--dir",
+        type=str,
+        default="",
+        help=f"Directory in cui salvare il file. [Default: {ROOT}]",
+    )
+    parser.add_argument(
+        "-n",
+        "--file-name",
+        type=str,
+        default=False,
+        help="Nome del file in cui salvare gli episdi. [RICHIESTO]",
+    )
+    args = parser.parse_args()
+    if args.url and args.file_name:
+        main(cmd=True)
+    else:
+        main(cmd=False)
